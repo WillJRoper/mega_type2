6871fb2c9b9d0097e49bf0aaee60252afd0d308f
wjr21 <wjr21@sussex.ac.uk>
2022-05-16 14:55:39 +0100

    Adjusting dynamic limiting

--- walk_links.py
+++ walk_links.py
@@ -196,7 +196,7 @@ def main_branch_length():
 
             H_max = np.max(H)
             if H_max > prev_max[i]:
-                ax.set_ylim(0.5, H_max + (0.1 * H_max))
+                ax.set_ylim(1, H_max + (0.2 * H_max))
             prev_max[i] = H_max
 
     # Label axes
@@ -276,7 +276,7 @@ def main_branch_length():
 
             H_max = np.max(H)
             if H_max > prev_max[i]:
-                ax.set_ylim(0.5, H_max + (0.1 * H_max))
+                ax.set_ylim(1, H_max + (0.2 * H_max))
             prev_max[i] = H_max
 
     # Label axes
