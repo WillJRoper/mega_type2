a90635ca51760bb0a7baa43bad3694ad84ccc4e0
wjr21 <wjr21@sussex.ac.uk>
2022-05-16 14:23:45 +0100

    Setting limits

--- walk_links.py
+++ walk_links.py
@@ -221,8 +221,9 @@ def main_branch_length():
 
     # Set y axis limits such that 0 is removed from the upper two subplots to
     # avoid tick stacking
-    # ax1.set_ylim(0.001, None)
-    # ax2.set_ylim(0.001, None)
+    ax1.set_ylim(None, 1.0)
+    ax2.set_ylim(None, 1.0)
+    ax3.set_ylim(None, 1.0)
 
     ax3.legend(loc='upper center',
                bbox_to_anchor=(0.5, -0.3),
@@ -301,8 +302,9 @@ def main_branch_length():
 
     # Set y axis limits such that 0 is removed from the upper two subplots to
     # avoid tick stacking
-    # ax1.set_ylim(0.001, None)
-    # ax2.set_ylim(0.001, None)
+    ax1.set_ylim(None, 1.0)
+    ax2.set_ylim(None, 1.0)
+    ax3.set_ylim(None, 1.0)
 
     ax3.legend(loc='upper center',
                bbox_to_anchor=(0.5, -0.3),
