4df0157b2854c6fc7384875026dcdbb8a39afcbc
wjr21 <wjr21@sussex.ac.uk>
2022-05-18 18:54:15 +0100

    Even more information

--- walk_links.py
+++ walk_links.py
@@ -171,8 +171,11 @@ def get_persist_lengths(reals, nparts, progs, descs):
                 snap += 1
 
             if npart >= 500 and (99 - root_snap) - length > 0:
-                print("Anomalous halo %d in snap %s walking from halo %d with length %d"
-                      % (prev_halo, str(snap - 1).zfill(4), ihalo, length))
+                print("Anomalous halo %d in snap %s walking from halo "
+                      "%d in root_snap %s with length %d, plength=%d"
+                      % (prev_halo, str(snap - 1).zfill(4), ihalo,
+                         str(root_snap).zfill(4), length,
+                         (99 - root_snap) - length > 0))
 
             # Appended this halos persistence length
             root_snaps.append(root_snap)
